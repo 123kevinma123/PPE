bbd3f68abf08190901c50df38510ac33f334f3c5
123kevinma123 <123kevinma123@gmail.com>
2024-03-28 00:14:45 -0400

    addtional website crawlers

--- client/src/CardSearch.js
+++ client/src/CardSearch.js
@@ -7,11 +7,13 @@ const pretty = require("pretty");
 
 
 function CardSearch() {
-    const [searchResults, setSearchResults] = useState([]);
+    const [searchResultsEbay, setSearchResultsEbay] = useState([]);
+    const [searchResultsWatchCount, setSearchResultsWatchCount] = useState([]);
 
     useEffect(() => {
         // Fetch eBay search results from server
         fetchEbaySearchResults();
+        fetchWatchCountSearchResults();
     }, []);
 
     const fetchEbaySearchResults = async () => {
@@ -21,19 +23,42 @@ function CardSearch() {
             const data = await response.json();
 
             // Set the fetched data as the search results state
-            setSearchResults(data);
+            setSearchResultsEbay(data);
         } catch (error) {
             console.error('Error fetching eBay data:', error);
         }
     };
+    const fetchWatchCountSearchResults = async () => {
+        try {
+            // Fetch watch count search results from server
+            const response = await fetch('http://localhost:1234/watch-count-search');
+            const data = await response.json();
+
+            // Set the fetched data as the search results state
+            setSearchResultsWatchCount(data);
+        } catch (error) {
+            console.error('Error fetching Watch Count data:', error);
+        }
+    };
+    const totalSum = searchResultsWatchCount.reduce((sum, result) => {
+        // Parse the price string to a float and add it to the sum
+        return sum + parseFloat(result.price.replace(/[^\d.]/g, ''));
+    }, 0);
+    
+    // Calculate the average price
+    const averagePrice = totalSum / searchResultsWatchCount.length;
+    
+    // Display the average price
+    console.log('Average Price:', averagePrice.toFixed(2));
 
     return (
         <div>
             {/* Render the fetched search results */}
-            {searchResults.map((result, index) => (
+            {searchResultsWatchCount.map((result, index) => (
                 <div key = {index} className = "results_list">
                     <h3>{result.title}</h3>
                     <p>Price: {result.price}</p>
+                    <p>Days since sale: {result.endTime}</p>
                     <a href = {result.url} target="_blank" rel="noopener noreferrer">View Item</a>
                 </div>
             ))}
--- server/index.js
+++ server/index.js
@@ -10,13 +10,51 @@ app.get("/", (req, res) => {
     res.send("hello world");
 });
 
+app.get("/watch-count-search", async (req, res) => {
+    try {
+        const browser = await puppeteer.launch();
+
+        const page = await browser.newPage();
+        await page.goto('http://www.watchcount.com/completed.php?bkw=%22cynthia%22+%22sv82%22+%22psa10%22&bcat=0&bcts=&sfsb=Show+Me%21&csbin=all&cssrt=ts&bfw=1&bslr=&bnp=&bxp=#serp');
+
+        await page.waitForSelector(".serptablecell2-adv");
+        
+        const results = await page.evaluate(() => {
+            const items = document.querySelectorAll(".serptablecell2-adv");
+            const data = [];
+            items.forEach(item => {
+                const title = item.querySelector(".valtitle.lovewrap.padr4 > span > a").textContent.trim();
+                const endTime = item.querySelector(".splittablecell1 .bhserp-dim2").textContent.trim();
+                const price = item.querySelector(".splittablecell1 .bhserp-new1").textContent.trim();
+                const url = item.querySelector(".underlinedlinks").href;
+                data.push({ title, endTime, price, url });
+            });
+            return data;
+        });
+        await browser.close();
+        const processed_data = [];
+        for (let i = 0; i < results.length; i++) {
+            const resultStr = results[i].title.toString();
+            if (resultStr.toLowerCase().includes("psa 10") && 
+                ! resultStr.toLowerCase().includes("japanese")
+            ) {
+                processed_data.push(results[i]);
+            }
+        }
+        //console.log(processed_data.length);
+        res.json(processed_data);
+    } catch (error) {
+        console.error('Error fetching watch count data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 app.get('/ebay-search', async (req, res) => {
     try {
         const browser = await puppeteer.launch();
 
         const page = await browser.newPage();
 
-        await page.goto('https://www.ebay.com/sch/i.html?_nkw="cynthia"+"hidden+fates"+"sv82"+"psa+10"');
+        await page.goto('https://www.ebay.com/sch/i.html?_from=R40&_nkw=%22cynthia%22+%22sv82%22+&_sacat=0&_ipg=240');
         
         await page.waitForSelector('.s-item');
         
@@ -24,15 +62,17 @@ app.get('/ebay-search', async (req, res) => {
             const items = document.querySelectorAll('.s-item');
             const data = [];
             items.forEach(item => {
-                const title = item.querySelector('.s-item__title').textContent.trim();
-                const price = item.querySelector('.s-item__price').textContent.trim();
-                const url = item.querySelector('.s-item__link').href;
+                const title = item.querySelector(".s-item__title").textContent.trim();
+                const price = item.querySelector(".s-item__price").textContent.trim();
+                const url = item.querySelector(".s-item__link").href;
                 data.push({ title, price, url });
             });
             return data;
         });
         await browser.close();
+        console.log(results.length);
         const processed_data = [];
+        //filter results here 
         for (let i = 1; i < results.length; i++) {
             const resultStr = results[i].title.toString();
             if (resultStr.toLowerCase().includes("psa 10") && 
@@ -41,7 +81,7 @@ app.get('/ebay-search', async (req, res) => {
                 processed_data.push(results[i]);
             }
         }
-        //dconsole.log(processed_data);
+        console.log(processed_data.length);
         res.json(processed_data);
     } catch (error) {
         console.error('Error fetching eBay data:', error);
