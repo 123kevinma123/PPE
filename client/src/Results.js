a0629f7be523224c7135ffcd1aed42da05ac2fd6
123kevinma123 <123kevinma123@gmail.com>
2024-06-08 18:04:46 -0400

    No results page + learning tailwind

--- client/src/Results.js
+++ client/src/Results.js
@@ -1,9 +1,11 @@
 import './Results.css';
 import CardOfTheDay from "./CardOfTheDay"
 import CardSearch from "./CardSearch"
+import sadpikachu from "C:/Users/123ke/OneDrive/Documents/Projects/react-app/client/src/sadpikachu.jpg"
 function Results({ returnResults }) {
     const placeholderImage = "https://fakeimg.pl/734x1024?text=No+Image";
     let content;
+    let noResults;
 
     try {
         content = returnResults.map((item, index) => (
@@ -25,15 +27,23 @@ function Results({ returnResults }) {
                 </div>
             </div>
         ));
+        noResults = <></>
     } catch (error) {
-        content = <div className = "no_results" />;
+        content = <></>;
+        noResults = (
+            <div className = "text-center text-black p-8 flex flex-col justify-center items-center">
+                <img src = {sadpikachu} alt = "No Results" className = "mb-4 "/>
+                <span className = "text-black font-semibold text-4xl mb-3">No Results :(</span>
+                <p className = "text-gray-600 text-2xl">Try searching for something else.</p>
+            </div>
+        )
     }
-
     return (
         <div className = "results_component">
             <div className = "results_title">
                 Search Results
             </div>
+            {noResults}
             <div className = "results_content">
                 {content}
             </div>
